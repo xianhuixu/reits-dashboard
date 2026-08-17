cf49f47e70bd926c706a724e4d3a64a52125b1f7
lion007 <xianhuixu@qq.com>
2026-08-17 17:06:50 +0800

    auto: update REITs dashboard 2026-08-17 17:06

--- deploy_cf.sh
+++ deploy_cf.sh
@@ -6,5 +6,6 @@ cd "$(dirname "$0")"
 rm -rf .cf-deploy
 mkdir .cf-deploy
 cp index.html data.js data_research.js news.js corp_actions.js projects.js inst_reits.js ui-utils.js .cf-deploy/
+cp 6015e57c6c228145fd65bb64b909526d.txt .cf-deploy/
 cp -r lib icons .cf-deploy/
 npx wrangler pages deploy .cf-deploy --project-name=reits-dashboard --branch=main --commit-dirty=true
